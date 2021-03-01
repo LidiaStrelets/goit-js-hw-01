e1fa8723bb6fc82cb90c34492cc01bb50eb8df67
LidiaStrelets <0630610171li@gmail.com>
2021-03-01 20:20:56 +0200

    ready

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,30 @@
-const total = 100;
-const ordered = 130;
-if (total < ordered) {
-    console.log('На складе недостаточно твоаров!');
-}
-else {
-    console.log('Заказ оформлен, с вами свяжется менеджер');
-}
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
